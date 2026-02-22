8d87957c4b035953ba9ffe5306c03e2430cc28c6
Jere Koivuranta <jere.koivuranta@gmail.com>
2026-02-22 15:01:13 +0200

    jatkettu

--- api/App.tsx
+++ api/App.tsx
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const URL = 'https://icanhazdadjoke.com/'
 
 export default function App() {
+  const [refresh, setRefresh] = useState<Boolean>(false)
+  const [joke, setJoke] = useState<String>('')
+  const [error, setError] = useState<String | null>(null)
+
+
+  useEffect(() => {
+    setError(null)
+    
+    fetch(URL, {
+      headers: {
+        'Contet-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin':'*',
+      },
+    })
+    .then(response => response.json())
+    .then(data => setJoke(data.joke))
+    .catch(error => setError('Error: ' + error))
+  }, [refresh])
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {!error && (
+        <>
+          <Text>{joke}</Text>
+          <Button onPress={() => setRefresh(!refresh)} title='New Joke'/>
+        </> 
+      )}
+      {error && (
+        <>
+          <Text>{error}</Text>
+          <Button onPress={() => setRefresh(!refresh)} title='Try Again'/>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
